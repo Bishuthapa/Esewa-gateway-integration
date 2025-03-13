03b899dd6efe6540f094b73d05155c05e6345fff
rizzer <bishu@Bishu>
2025-03-13 17:48:32 +0545

    create schemas

--- ride/src/model/User.ts
+++ ride/src/model/User.ts
@@ -18,3 +18,67 @@ const MessageSchema: Schema<Message> = new Schema({
     }
 
 })
+
+
+export interface User extends Document {
+    userName: string;
+    email : string;
+    password : string;
+    verifyedCode: string;
+    verifycodeExpiration : Date;
+    isVerified : boolean;
+    isAcceptingMessages : boolean;
+    messages : Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+    userName : {
+        type : String,
+        required : [true, "userName is required"],
+    },
+    email : {
+        type: String,
+        required : [true, "email is required"],
+        unique : true,
+        match : [/.+@.+\..+/, "Please enter a valid email address"] 
+
+    },
+
+    password : {
+        type: String,
+        required :true, 
+        match: [/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, "Please enter a valid password"]
+    },
+
+    verifyedCode : {
+        type: String,
+        required :[true, "verify code is required"], 
+
+        
+    },
+    verifycodeExpiration : {
+        type: Date,
+        required :[true, "verify code expire date is required"], 
+        
+    },
+    
+    isVerified : {
+        type: Boolean,
+        default : false,
+        
+    },
+
+    isAcceptingMessages : {
+        type: Boolean,
+        required :true,
+    },
+
+    messages :
+         [MessageSchema],
+    
+
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
